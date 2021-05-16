203c880a095241a25fa1268ad3962ad17d3b34ab
parvanescu <parvanescu.stefan@yahoo.com>
2021-05-17 00:04:48 +0300

    Added main-page and nav-bar

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -11,6 +11,9 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -27,6 +30,9 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatIconModule,
     MatSidenavModule,
     MatExpansionModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  conferences = [{name: 'Conference1', edition: 'Edition1' , startDate: '01-01-2000' , endDate: '02-01-2000' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend leo lorem, vitae maximus elit volutpat quis. Quisque tempor nulla sit amet ipsum viverra pulvinar. Aenean lobortis tortor turpis, sit amet ornare diam euismod ut. Quisque pharetra felis vel porttitor tincidunt. Proin id erat molestie, commodo quam sed, scelerisque augue. Etiam vulputate tellus mi, quis fringilla felis interdum et. Nunc felis sapien, tincidunt nec lacus vel, gravida vulputate odio. Aliquam maximus risus eu dolor volutpat, nec vehicula nunc blandit. Morbi diam lacus, placerat sed felis id, posuere pretium velit.'},
+    {name: 'Conference1', edition: 'Edition1' , startDate: '01-01-2000' , endDate: '02-01-2000' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend leo lorem, vitae maximus elit volutpat quis. Quisque tempor nulla sit amet ipsum viverra pulvinar. Aenean lobortis tortor turpis, sit amet ornare diam euismod ut. Quisque pharetra felis vel porttitor tincidunt. Proin id erat molestie, commodo quam sed, scelerisque augue. Etiam vulputate tellus mi, quis fringilla felis interdum et. Nunc felis sapien, tincidunt nec lacus vel, gravida vulputate odio. Aliquam maximus risus eu dolor volutpat, nec vehicula nunc blandit. Morbi diam lacus, placerat sed felis id, posuere pretium velit.' },
+    {name: 'Conference1', edition: 'Edition1' , startDate: '01-01-2000' , endDate: '02-01-2000' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend leo lorem, vitae maximus elit volutpat quis. Quisque tempor nulla sit amet ipsum viverra pulvinar. Aenean lobortis tortor turpis, sit amet ornare diam euismod ut. Quisque pharetra felis vel porttitor tincidunt. Proin id erat molestie, commodo quam sed, scelerisque augue. Etiam vulputate tellus mi, quis fringilla felis interdum et. Nunc felis sapien, tincidunt nec lacus vel, gravida vulputate odio. Aliquam maximus risus eu dolor volutpat, nec vehicula nunc blandit. Morbi diam lacus, placerat sed felis id, posuere pretium velit.' },
+    {name: 'Conference1', edition: 'Edition1' , startDate: '01-01-2000' , endDate: '02-01-2000' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend leo lorem, vitae maximus elit volutpat quis. Quisque tempor nulla sit amet ipsum viverra pulvinar. Aenean lobortis tortor turpis, sit amet ornare diam euismod ut. Quisque pharetra felis vel porttitor tincidunt. Proin id erat molestie, commodo quam sed, scelerisque augue. Etiam vulputate tellus mi, quis fringilla felis interdum et. Nunc felis sapien, tincidunt nec lacus vel, gravida vulputate odio. Aliquam maximus risus eu dolor volutpat, nec vehicula nunc blandit. Morbi diam lacus, placerat sed felis id, posuere pretium velit.' },
+  ];
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  printHere() {
+    console.log("here");
+  }
 }
