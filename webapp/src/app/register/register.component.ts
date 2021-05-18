b2170f9230dd84088e5f176b5241f68a358997d0
carinapetcu <petcu.carina07@gmail.com>
2021-05-18 12:01:45 +0300

    Added user model and service

--- webapp/src/app/register/register.component.ts
+++ webapp/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CMSUserService} from '../service/cmsuser.service';
+import {CMSUser} from '../domain/cmsuser.model';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   hide = true;
+  successfullyAdd: boolean;
 
-  constructor() { }
+  constructor(private cmsuserService: CMSUserService) {
+    this.successfullyAdd = true;
+  }
 
   ngOnInit(): void {
+  }
+
+  addUser(fullName: string, email: string, username: string, password: string): void{
+    const user: CMSUser = {id: 0, fullName, email, username, password} as CMSUser;
+    this.cmsuserService.addUser(user).subscribe(
+      response => {
+        this.successfullyAdd = response.status === 200;
+      }
+    );
   }
 
 }
