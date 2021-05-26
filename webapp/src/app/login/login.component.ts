aaa27af3ef688c58cb58bdf00fee59eb224cb5d0
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2021-05-26 23:03:18 +0300

    login

--- webapp/src/app/login/login.component.ts
+++ webapp/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {CMSUserService} from '../service/cmsuser.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   hide = true;
-  constructor() { }
+
+  loginError = '';
+
+  loginForm = this.formBuilder.group({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  constructor( private cmsUserService: CMSUserService,
+               private formBuilder: FormBuilder,
+               private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    this.cmsUserService.login(this.loginForm.controls.username.value,
+      this.loginForm.controls.password.value)
+      .subscribe(
+        response => {
+          this.router.navigateByUrl('/home');
+        },
+        response => {
+          alert(response.error);
+        }
+      );
+  }
 }
--- webapp/src/app/service/cmsuser.service.ts
+++ webapp/src/app/service/cmsuser.service.ts
@@ -1,14 +1,18 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import {CMSUser} from '../domain/cmsuser.model';
 import {Observable} from 'rxjs';
 
 @Injectable()
 export class CMSUserService {
   private userUrl = 'http://localhost:8080/api/users';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
 
   constructor(private httpClient: HttpClient) {
   }
+
 
   addUser(user: CMSUser): Observable<HttpResponse<any>> {
     const url = `${this.userUrl}/add`;
@@ -19,6 +23,17 @@ export class CMSUserService {
     const url = `${this.userUrl}/${id}`;
     return this.httpClient.get<HttpResponse<any>>(url);
   }
+
+  login(username: string, password: string): Observable<any>{
+    const url = `${this.userUrl}/login`;
+    const loginInfo = {
+      username,
+      password
+    };
+
+    return this.httpClient.post(url, loginInfo, this.httpOptions);
+  }
+
 
 }
 
